d290261e4c7393804f630293769cb9639c2ec86a
Zakaria Fichtali <92021986+ZakariaFichtali@users.noreply.github.com>
2023-04-06 01:02:35 +0000

    v1

---
+++ app/src/main/java/com/sanfor01/myguide1/SecActivity.java
@@ -0,0 +1,78 @@
+package com.sanfor01.myguide1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class SecActivity extends AppCompatActivity {
+
+    TextView play1, inv_friends, rateApp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sec);
+
+        View play1 = findViewById(R.id.play1);
+        View inv_friends = findViewById(R.id.inv_friends);
+        View rateApp = findViewById(R.id.rateapp);
+
+
+        play1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://play.google.com")));
+            }
+        });
+
+        inv_friends.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
+                sharingIntent.setType("text/plain");
+                String shareBody = "Here is the share content body";
+                sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Subject Here");
+                sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, shareBody);
+                startActivity(Intent.createChooser(sharingIntent, "Share via"));
+
+            }
+        });
+
+
+        rateApp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                try {
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + getPackageName())));
+                } catch (android.content.ActivityNotFoundException anfe) {
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://play.google.com/store/apps/details?id=" + getPackageName())));
+                }
+            }
+        });
+
+
+
+
+
+
+
+
+
+
+
+        View letsPlay = findViewById(R.id.back1);
+        letsPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent back1 = new Intent(SecActivity.this, MainActivity.class);
+                startActivity(back1);
+            }
+        });
+    }
+}
